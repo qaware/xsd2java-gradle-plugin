eca44b7ffab3efd871d2eb27f7d1dfb4cbc3c053
Christian Fritz <christian.fritz@qaware.de>
2018-03-19 13:06:22 +0100

    Adds comments for the xsd2java plugin extension.

--- src/main/groovy/de/qaware/gradle/plugin/xsd2java/xjc/Xsd2JavaPluginExtension.groovy
+++ src/main/groovy/de/qaware/gradle/plugin/xsd2java/xjc/Xsd2JavaPluginExtension.groovy
@@ -14,13 +14,36 @@
  *    limitations under the License.
  */
 package de.qaware.gradle.plugin.xsd2java.xjc
-
+/**
+ * The extension object for the build.gradle script to configure the xsd2java task.
+ */
 class Xsd2JavaPluginExtension {
+    /**
+     * The base directory where the xsd schemas are located.
+     */
     String schemaDirPath = 'src/main/resources/'
+    /**
+     * The package name for all the generated java sources.
+     */
     String packageName
+    /**
+     * Should add the xjc configuration to the ant runner classpath.
+     */
     Boolean extension = false
+    /**
+     * Additional arguments for the xsd2java ant task.
+     */
     def arguments = []
+    /**
+     * Do not generate these classes. They are user defined.
+     */
     String userDefinedClasses = ""
-    def inputDir = project.file("$project.projectDir/src/main/resources")
+    /**
+     * The full base directory where the xsd schemas are located.
+     */
+    def inputDir = project.file("$project.projectDir/${schemaDirPath}")
+    /**
+     * The target path write the java sources to.
+     */
     def outputDir = project.file("$project.buildDir/generated-sources/xjc")
 }
